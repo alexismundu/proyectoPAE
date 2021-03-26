ff942baf32957c0fffe7359822bde971f5af2cd3
Dworak <esteban.cervantes@resser.com>
2021-03-25 21:54:20 -0600

    feat: proof of concept: FE and BE connection

---
+++ client/src/app/users/users.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss'],
+})
+export class UsersComponent implements OnInit {
+  li: any;
+  lis: any[] = [];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:3000/users')
+    .subscribe(Response => {
+  
+      // If response comes hideloader() function is called
+      // to hide that loader 
+      if(Response){  
+        hideloader();
+      }
+      this.li=Response;
+      this.lis=this.li.users
+      console.log(this.li.users)
+    });
+    function hideloader(){
+      // @ts-ignore
+      document.getElementById('loading').style.display = 'none';
+  }}
+}
